37f685fb3d2b35a4595e4302242ea637bed18867
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-04-25 12:51:07 +0100

    Working on parsing systmOne input

--- src/lib/parsers/parseSystmOne.js
+++ src/lib/parsers/parseSystmOne.js
@@ -2,11 +2,11 @@ const parseSystmOne = (files)=>{
    // console.log(typeof(files))
    
    let count = 0;
-   let reportOne;
-   let reportTwo;
-   let reportThree;
+   // let reportOne;
+   // let reportTwo;
+   // let reportThree;
    
-
+   const reports = {}
    for (let i = 0; i < files.length; i++){
       let file = files[i]
       const reader = new FileReader()
@@ -15,16 +15,67 @@ const parseSystmOne = (files)=>{
          const lines = reader.result.split('\n')
          const headerArray = lines[0].split(',');
          console.log(headerArray)
+         // console.log(lines[1])
          count++;
+
+         
          if (headerArray[0] === "Full Name" && headerArray[1] === 'Age'){
-            reportOne = file 
+            reports["report1"] = file
+           
          }
          else if (headerArray[0] === "NHS number" && headerArray[1] === 'Frailty'){
-            reportTwo = file 
+            reports["report2"] = file
          }
-         else if (headerArray[0] === "NHS number" && headerArray[1] === 'Antiplatelet'){
-            reportThree = file 
+         else{
+            reports["report3"] = file
          }
+         
+         const parsed_data = []
+        
+         //Parse each file.
+         if (count === 3){
+            
+            const reader = new FileReader();
+
+            reader.onload = () => {
+               const report_string = reader.result
+               const report_array = report_string.split('\n')
+               const report_header_array = report_array[0].split(',')
+               console.log(report_header_array)
+
+               
+               for(let i = 1; i < report_array.length; i++){
+                  // report_array[i].split(',')
+                  let row_array = report_array[i].split(',')
+                  const report_object = {}
+                  for (let i = 0; i < row_array.length; i++){ 
+                     
+                     report_object[report_header_array[i]] = row_array[i]
+                     
+                  }
+                  parsed_data.push(report_object)
+               }
+               console.log(parsed_data)
+            }
+            
+            
+            
+            
+            
+            reader.onerror = ()=>{
+               console.log(reader.error)
+            }
+
+            reader.readAsText(reports['report1'])
+            // console.log(reports['report1'])
+
+         }
+         
+
+
+
+
+
       }
 
       reader.onerror=()=>{
