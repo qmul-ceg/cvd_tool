d58bc510bca0769e81df3d84007f2ab22644706d
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-05-01 14:27:43 +0100

    working on parsing systm one reports

--- src/lib/parsers/parseSystmOne.js
+++ src/lib/parsers/parseSystmOne.js
@@ -1,126 +1,112 @@
 const parseSystmOne = (files)=>{
-   // console.log(typeof(files))
+
    const masterReport = {}
    let count = 0; 
-   const reports = {}
-   for (let i = 0; i < files.length; i++){
-      let file = files[i]
-      const reader = new FileReader()
-      
-      reader.onload = () => {
-         const lines = reader.result.split('\n')
-         const headerArray = lines[0].split(',');
-         // console.log(headerArray)
-         // console.log(lines[1])
-         count++;
-
-         
-         if (headerArray[0] === "Full Name" && headerArray[1] === 'Age'){
-            reports["report1"] = file
-           
-         }
-         else if (headerArray[0] === "NHS number" && headerArray[1] === 'Frailty'){
-            reports["report2"] = file
-         }
-         else{
-            reports["report3"] = file
-         }
-         
-
-         const parsed_reports = []
-         let count_two = 0
-         //Parse each file.
-         if (count === 3){
-            
-            
-            for (const report in reports){
-
-               const reader = new FileReader();
-               reader.onload = () => {
-                  const report_string = reader.result //Report as a string
-                  const report_array = report_string.split('\n') //Return an array of the string splitting each one at the new line
-                  const report_header_array = report_array[0].split(',')
-
-                  const report_object_array = []
-                  for(let i = 1; i < report_array.length; i++){
-                     let row_array = report_array[i].split(',')
-                     const report_object = {}    
-                     for (let i = 0; i < row_array.length; i++){
-                        report_object[report_header_array[i]] = row_array[i] 
-                     }
-                     report_object_array.push(report_object) 
-                  } 
-                  parsed_reports.push(report_object_array)
-                  count_two++
+   const reports = {
+      report1: null,
+      report2: null,
+      report3: null
+   }
+   
+   const firstPromise = new Promise((resolve, reject) => {
+      for (let i = 0; i < files.length; i++){
+         let file = files[i]
+               const reader = new FileReader()
                
-                  console.log(parsed_reports)
+               reader.onload = () => {
+                  const lines = reader.result.split('\n')
+                  const headerArray = lines[0].split(',');
+                  count++;
 
-                  if(count_two == 3 ){
-                     for (const report of parsed_reports){
-                        for (const object of report){
-                           // const keys = Object.keys(object)
+                  
+                  if (headerArray[0] === "Full Name" && headerArray[1] === 'Age'){
+                     reports["report1"] = file
+                  
+                  }
+                  else if (headerArray[0] === "NHS number" && headerArray[1] === 'Frailty'){
+                     reports["report2"] = file
+                  }
+                  else{
+                     reports["report3"] = file
+                  }
 
-                           for (const [key, value] of Object.entries(object)){
-                              const nhsKey = 'NHS number'
-                              if(object[nhsKey]) {
-                                 console.log(object[nhsKey])
-                              } 
-                              return; 
-                              
+                  if(Object.values(reports).every(value => value)){
+                     resolve(reports)
+                  }
+
+                  //LOOK AT REPORTS
+                  
+                  const parsed_reports = []
+                  let count_two = 0
+                  //Parse each file.
+                  if (count === 3){
+                     
+                     for (const report in reports){
+
+                        const reader = new FileReader();
+                        reader.onload = () => {
+                           const report_string = reader.result //Report as a string
+                           const report_array = report_string.split('\n') //Return an array of the string splitting each one at the new line
+                           const report_header_array = report_array[0].split(',')
+                           
+                           //Changing headers for report 3 
+                           if(report_header_array[1].trim() === "Antiplatelet"){
+                              for (let i = 0; i < report_header_array.length; i++){
+                                 if (report_header_array[i] === "Date of issue"){
+                                    const medicationName = report_header_array[i-1]
+                                    report_header_array[i] = `${report_header_array[i]} - ${medicationName}`.trim()
+                                    // console.log(report_header_array[i])
+                                 }
+                              }
                            }
+                           // console.log(report_header_array)
+                  
+                           const report_object_array = []
+                           for(let i = 1; i < report_array.length; i++){
+                              let row_array = report_array[i].split(',')
+                              const report_object = {}    
+                              for (let i = 0; i < row_array.length; i++){
+                                 report_object[report_header_array[i]] = row_array[i] 
+                              }
+                              report_object_array.push(report_object) 
+                           } 
+                           parsed_reports.push(report_object_array)
+                           count_two++
+                        
+                           if(count_two == 3 ){
+                              for (const report of parsed_reports){
+                                 for (const object of report){
+                                    const nhs_number_check = object['NHS number']
+                                    if(nhs_number_check){
+                                       if(Object.hasOwn(masterReport, nhs_number_check)){
+                                          Object.assign(masterReport[nhs_number_check], object)
+                                       }
+                                       else{
+                                          masterReport[nhs_number_check] = object
+                                       }
+                                    } 
+                                 }
+                              }
+                           } 
                         }
                         
-                        // for (const object in report){
-                        //    console.log(object)
-                           // for (const [key, value] of Object.entries(object)){
-                           //    console.log(key + value)
-                           //    return
-                           // }
-                           // let key = 'NHS number'
-                           //    if (object[key]){
-                           //       console.log(object[key])
-                           //       return
-                           //    }
-                                 
+                        reader.onerror = ()=>{
+                           console.log(reader.error)
                         }
-                     
+                        reader.readAsText(reports[report])       
+                     }
+                     return masterReport
                   }
-                  
-               //loop through reports list
-                           
-                              //loop through each array of objects
-                                 //check for the NHS number check if it is true
-                                    // check if it is an object key in the master object
-                                       // if it is add the row to the object
-                                       // it is not add the use the key as an object
-
-                  
                }
-               
-               reader.onerror = ()=>{
+
+               reader.onerror=()=>{
                   console.log(reader.error)
                }
 
-               reader.readAsText(reports[report])
-               
-               
-               
-            }
-
-
-           
-            
-
-
-         }
-      }
-
-      reader.onerror=()=>{
-         console.log(reader.error)
-      }
-      reader.readAsText(file)
-   }
-
+               reader.readAsText(file)
+   } 
+})
+  
 }
 
 export default parseSystmOne
